e5af11f9ad3add9044d39241260a8993f2f480c4
Yehuda Katz <wycats@gmail.com>
2019-01-13 08:47:03 -0800

    Fix plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,222 +3,228 @@ module.exports = {
   description: "Just playing around",
   base: "/ember-next-experiments",
   plugins: [
-    (options, ctx) => {
-      const Prism = require("prismjs");
-      const loadLanguages = require("prismjs/components/");
-      loadLanguages(["javascript", "typescript", "php"]);
+    [
+      "vuepress-gbs-syntax",
+      (options, ctx) => {
+        const Prism = require("prismjs");
+        const loadLanguages = require("prismjs/components/");
+        loadLanguages(["javascript", "typescript", "php"]);
 
-      Prism.languages.hbshtml = {
-        comment: /<!--[\s\S]*?-->/,
-        prolog: /<\?[\s\S]+?\?>/,
-        doctype: /<!DOCTYPE[\s\S]+?>/i,
-        cdata: /<!\[CDATA\[[\s\S]*?]]>/i,
-        "whole-tag": {
-          pattern: /<\/?(?!\d)[^\s>\/=$<%]+(?:\s+[^\s>\/=]+(?:=(?:("|')(?:\\[\s\S]|(?!\1)[^\\])*\1|[^\s'">=]+))?)*\s*\/?>/i,
-          greedy: true,
-          inside: {
-            tag: {
-              pattern: /^<\/?[^\s>\/]+/i,
-              inside: {
-                punctuation: /^<\/?/,
-                namespace: /^[^\s>\/:]+:/
-              }
-            },
-            whitespace: /^\s+/,
-            arg: {
-              pattern: /^@[^\s>\/]+=\{\{.*?\}\}/,
-              inside: {
-                "arg-name": {
-                  pattern: /^@[^=]+/,
-                  alias: ["symbol", "italic"]
-                },
-                equals: { pattern: /^=/, alias: "punctuation" },
-                "arg-value": {
-                  pattern: /^.*/i,
-                  inside: Prism.languages.htmlbars
-                }
-              }
-            },
-
-            stringArg: {
-              pattern: /^@[^\s>\/]+="[^"]*"/,
-              inside: {
-                "arg-name": {
-                  pattern: /^@[^=]+/,
-                  alias: ["symbol", "italic"]
-                },
-                equals: { pattern: /^=/, alias: "punctuation" },
-                string: {
-                  pattern: /^.*/i
-                }
-              }
-            },
-
-            "attr-value": {
-              pattern: /=(?:("|')(?:\\[\s\S]|(?!\1)[^\\])*\1|[^\s'">=]+)/i,
-              inside: {
-                punctuation: /^=/,
-                string: /^.*/
-              }
-            },
-
-            punctuation: /\/?>/,
-
-            "attribute-name": {
-              pattern: /[^\s>\/@]+/,
-              alias: ["italic", "comment"],
-              inside: {
-                namespace: /^[^\s>\/:]+:/
-              }
-            }
-          }
-        },
-        entity: /&#?[\da-z]{1,8};/i
-      };
-
-      Prism.languages.markup["tag"].inside["attr-value"].inside["entity"] =
-        Prism.languages.markup["entity"];
-
-      let hbsIdent = /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/;
-
-      let inMustache = {
-        pattern: /^[^}]+/,
-        inside: {
-          keyword: /^\s*as\s*(?=|)/,
-
-          hash: {
-            pattern: /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+=/,
+        Prism.languages.hbshtml = {
+          comment: /<!--[\s\S]*?-->/,
+          prolog: /<\?[\s\S]+?\?>/,
+          doctype: /<!DOCTYPE[\s\S]+?>/i,
+          cdata: /<!\[CDATA\[[\s\S]*?]]>/i,
+          "whole-tag": {
+            pattern: /<\/?(?!\d)[^\s>\/=$<%]+(?:\s+[^\s>\/=]+(?:=(?:("|')(?:\\[\s\S]|(?!\1)[^\\])*\1|[^\s'">=]+))?)*\s*\/?>/i,
+            greedy: true,
             inside: {
-              name: {
-                pattern: hbsIdent,
-                alias: ["symbol", "italic"]
+              tag: {
+                pattern: /^<\/?[^\s>\/]+/i,
+                inside: {
+                  punctuation: /^<\/?/,
+                  namespace: /^[^\s>\/:]+:/
+                }
               },
-              punctuation: /^=/
-            }
-          },
-          string: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
-          number: /\b0x[\dA-Fa-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:[Ee][+-]?\d+)?/,
-          boolean: /\b(?:true|false)\b/,
-          block: {
-            pattern: /^(\s*~?\s*)[#\/]\S+?(?=\s*~?\s*$|\s)/i,
-            lookbehind: true,
-            alias: "keyword"
-          },
-          brackets: {
-            pattern: /\[[^\]]+\]/,
-            inside: {
-              punctuation: /\[|\]/,
-              variable: /[\s\S]+/
-            }
-          },
-          punctuation: /[!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
-          variable: hbsIdent
-        }
-      };
+              whitespace: /^\s+/,
+              arg: {
+                pattern: /^@[^\s>\/]+=\{\{.*?\}\}/,
+                inside: {
+                  "arg-name": {
+                    pattern: /^@[^=]+/,
+                    alias: ["symbol", "italic"]
+                  },
+                  equals: { pattern: /^=/, alias: "punctuation" },
+                  "arg-value": {
+                    pattern: /^.*/i,
+                    inside: Prism.languages.htmlbars
+                  }
+                }
+              },
 
-      Prism.languages.htmlbars = Prism.languages.extend("hbshtml", {
-        comment: /\{\{![\s\S]*?\}\}/,
-        block: {
-          pattern: /\{\{[#/][\s\S]*?\}\}/,
+              stringArg: {
+                pattern: /^@[^\s>\/]+="[^"]*"/,
+                inside: {
+                  "arg-name": {
+                    pattern: /^@[^=]+/,
+                    alias: ["symbol", "italic"]
+                  },
+                  equals: { pattern: /^=/, alias: "punctuation" },
+                  string: {
+                    pattern: /^.*/i
+                  }
+                }
+              },
+
+              "attr-value": {
+                pattern: /=(?:("|')(?:\\[\s\S]|(?!\1)[^\\])*\1|[^\s'">=]+)/i,
+                inside: {
+                  punctuation: /^=/,
+                  string: /^.*/
+                }
+              },
+
+              punctuation: /\/?>/,
+
+              "attribute-name": {
+                pattern: /[^\s>\/@]+/,
+                alias: ["italic", "comment"],
+                inside: {
+                  namespace: /^[^\s>\/:]+:/
+                }
+              }
+            }
+          },
+          entity: /&#?[\da-z]{1,8};/i
+        };
+
+        Prism.languages.markup["tag"].inside["attr-value"].inside["entity"] =
+          Prism.languages.markup["entity"];
+
+        let hbsIdent = /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/;
+
+        let inMustache = {
+          pattern: /^[^}]+/,
           inside: {
-            start: { pattern: /^\{\{[#/]/, alias: ["delimiter", "important"] },
-            function: /^[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/,
-            contents: inMustache,
-            end: { pattern: /^\}\}/, alias: ["delimiter", "important"] }
-          }
-        },
-        mustache: {
-          pattern: /\{\{[\s\S]*?\}\}/,
-          inside: {
-            start: { pattern: /^\{\{/, alias: ["delimiter", "important"] },
-            when: {
-              pattern: /^when\s+:[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/,
+            keyword: /^\s*as\s*(?=|)/,
+
+            hash: {
+              pattern: /[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+=/,
               inside: {
-                function: /^when/,
-                symbol: /^.*/
+                name: {
+                  pattern: hbsIdent,
+                  alias: ["symbol", "italic"]
+                },
+                punctuation: /^=/
               }
             },
-            function: /^[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+(?=[ \}])/,
-            contents: inMustache,
-            end: { pattern: /^\}\}/, alias: ["delimiter", "important"] }
-          }
-        }
-      });
-
-      Prism.languages.gbs = {
-        hbsOnly: {
-          pattern: /^((?!---)[\s\S])*$/,
-          inside: {
-            hbs: {
-              alias: "language-handlebars",
-              pattern: /^[\s\S]*/,
-              inside: Prism.languages.htmlbars
-            }
-          }
-        },
-
-        jsComment: {
-          pattern: /--- js ---[\s\S]*?(?=---)/,
-          inside: {
-            head: {
-              pattern: /^--- js ---/,
-              alias: ["function-name", "type-header", "italic"]
-            },
-            js: {
-              pattern: /^[\s\S]*/,
+            string: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
+            number: /\b0x[\dA-Fa-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:[Ee][+-]?\d+)?/,
+            boolean: /\b(?:true|false)\b/,
+            block: {
+              pattern: /^(\s*~?\s*)[#\/]\S+?(?=\s*~?\s*$|\s)/i,
               lookbehind: true,
-              alias: "language-js",
-              inside: Prism.languages.javascript
-            }
-          }
-        },
-
-        tsComment: {
-          pattern: /--- ts ---[\s\S]*?(?=---)/,
-          inside: {
-            head: {
-              pattern: /^--- ts ---/,
-              alias: ["function-name", "type-header", "italic"]
+              alias: "keyword"
             },
-            ts: {
-              alias: "language-ts",
-              pattern: /^[\s\S]*/,
-              inside: Prism.languages.typescript
-            }
-          }
-        },
-
-        hbsComment: {
-          pattern: /--- hbs ---[\s\S]*$/,
-          inside: {
-            head: {
-              pattern: /^--- hbs ---/,
-              alias: ["function-name", "type-header", "italic"]
+            brackets: {
+              pattern: /\[[^\]]+\]/,
+              inside: {
+                punctuation: /\[|\]/,
+                variable: /[\s\S]+/
+              }
             },
-            hbs: {
-              alias: "language-handlebars",
-              pattern: /^[\s\S]*/,
-              inside: Prism.languages.htmlbars
-            }
+            punctuation: /[!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~]/,
+            variable: hbsIdent
           }
-        },
+        };
 
-        phpComment: {
-          pattern: /--- php[\s\S]*$/,
-          alias: "function-name",
-          inside: {
-            head: {
-              pattern: /^--- php/,
-              alias: "function-name"
-            },
-            php: {
-              pattern: /^[\s\S]*/,
-              lookbehind: true,
-              inside: Prism.languages.php
+        Prism.languages.htmlbars = Prism.languages.extend("hbshtml", {
+          comment: /\{\{![\s\S]*?\}\}/,
+          block: {
+            pattern: /\{\{[#/][\s\S]*?\}\}/,
+            inside: {
+              start: {
+                pattern: /^\{\{[#/]/,
+                alias: ["delimiter", "important"]
+              },
+              function: /^[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/,
+              contents: inMustache,
+              end: { pattern: /^\}\}/, alias: ["delimiter", "important"] }
+            }
+          },
+          mustache: {
+            pattern: /\{\{[\s\S]*?\}\}/,
+            inside: {
+              start: { pattern: /^\{\{/, alias: ["delimiter", "important"] },
+              when: {
+                pattern: /^when\s+:[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+/,
+                inside: {
+                  function: /^when/,
+                  symbol: /^.*/
+                }
+              },
+              function: /^[^!"#%&'()*+,.\/;<=>@\[\\\]^`{|}~\s]+(?=[ \}])/,
+              contents: inMustache,
+              end: { pattern: /^\}\}/, alias: ["delimiter", "important"] }
             }
           }
-        }
-      };
-    }
+        });
+
+        Prism.languages.gbs = {
+          hbsOnly: {
+            pattern: /^((?!---)[\s\S])*$/,
+            inside: {
+              hbs: {
+                alias: "language-handlebars",
+                pattern: /^[\s\S]*/,
+                inside: Prism.languages.htmlbars
+              }
+            }
+          },
+
+          jsComment: {
+            pattern: /--- js ---[\s\S]*?(?=---)/,
+            inside: {
+              head: {
+                pattern: /^--- js ---/,
+                alias: ["function-name", "type-header", "italic"]
+              },
+              js: {
+                pattern: /^[\s\S]*/,
+                lookbehind: true,
+                alias: "language-js",
+                inside: Prism.languages.javascript
+              }
+            }
+          },
+
+          tsComment: {
+            pattern: /--- ts ---[\s\S]*?(?=---)/,
+            inside: {
+              head: {
+                pattern: /^--- ts ---/,
+                alias: ["function-name", "type-header", "italic"]
+              },
+              ts: {
+                alias: "language-ts",
+                pattern: /^[\s\S]*/,
+                inside: Prism.languages.typescript
+              }
+            }
+          },
+
+          hbsComment: {
+            pattern: /--- hbs ---[\s\S]*$/,
+            inside: {
+              head: {
+                pattern: /^--- hbs ---/,
+                alias: ["function-name", "type-header", "italic"]
+              },
+              hbs: {
+                alias: "language-handlebars",
+                pattern: /^[\s\S]*/,
+                inside: Prism.languages.htmlbars
+              }
+            }
+          },
+
+          phpComment: {
+            pattern: /--- php[\s\S]*$/,
+            alias: "function-name",
+            inside: {
+              head: {
+                pattern: /^--- php/,
+                alias: "function-name"
+              },
+              php: {
+                pattern: /^[\s\S]*/,
+                lookbehind: true,
+                inside: Prism.languages.php
+              }
+            }
+          }
+        };
+      }
+    ]
   ]
 };
